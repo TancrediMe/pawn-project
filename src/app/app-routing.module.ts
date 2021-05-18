c08fe138f3fda1a95bd9c54ed9ca5158fa9ee0b3
TancrediMe <tancredi.meli@studenti.unicam.it>
2021-05-19 00:10:10 +0200

    Aggiunta scheletro form per nuova immatricolazione

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaImmatricolazioniComponent } from './components/lista-immatricolazioni/lista-immatricolazioni.component';
+import { NuovaImmatricolazioneComponent } from './components/nuova-immatricolazione/nuova-immatricolazione.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'lista-immatricolazioni', pathMatch: 'full'},
+  { path: 'lista-immatricolazioni', component: ListaImmatricolazioniComponent},
+  { path: 'nuova-immatricolazione', component: NuovaImmatricolazioneComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
