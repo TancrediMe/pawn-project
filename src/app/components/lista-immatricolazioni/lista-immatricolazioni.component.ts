757354ffceb9972d885493006d892fd91b1667fb
TancrediMe <tancredi.meli@studenti.unicam.it>
2021-05-20 00:36:08 +0200

    Implementazione getLista

--- src/app/components/lista-immatricolazioni/lista-immatricolazioni.component.ts
+++ src/app/components/lista-immatricolazioni/lista-immatricolazioni.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ImmatricolazioneService } from 'src/app/services/immatricolazione.service';
 
 @Component({
   selector: 'app-lista-immatricolazioni',
   templateUrl: './lista-immatricolazioni.component.html',
-  styleUrls: ['./lista-immatricolazioni.component.css']
+  styleUrls: ['./lista-immatricolazioni.component.css'],
 })
 export class ListaImmatricolazioniComponent implements OnInit {
+  immatricolazione: any[] = [];
 
-  constructor() { }
+  constructor(private _immatricolazioneService: ImmatricolazioneService) {}
 
   ngOnInit(): void {
+    this.getLista();
   }
 
+  getLista() {
+    this._immatricolazioneService.getLista().subscribe((data) => {
+      this.immatricolazione = [];
+      data.forEach((element: any) => {
+        this.immatricolazione.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data(),
+        });
+      });
+      console.log(this.immatricolazione);
+    });
+  }
 }
