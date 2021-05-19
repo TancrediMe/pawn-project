0b33e93eb7ef957e07fa179e44c44d5eef2f939c
TancrediMe <tancredi.meli@studenti.unicam.it>
2021-05-19 09:32:49 +0200

    Aggiunta moduli firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//Moduli
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+//Componenti
 import { AppComponent } from './app.component';
 import { ListaImmatricolazioniComponent } from './components/lista-immatricolazioni/lista-immatricolazioni.component';
 import { NuovaImmatricolazioneComponent } from './components/nuova-immatricolazione/nuova-immatricolazione.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
