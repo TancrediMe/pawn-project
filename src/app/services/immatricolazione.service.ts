856ae7bf0f62e2b2f5fa8c2fb6f32f6ae5ce8816
TancrediMe <tancredi.meli@studenti.unicam.it>
2021-05-20 00:27:32 +0200

    Aggiunta metodo getLista per le immatricolazioni

--- src/app/services/immatricolazione.service.ts
+++ src/app/services/immatricolazione.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +10,9 @@ export class ImmatricolazioneService {
 
   salvaLista(immatricolazione: any): Promise<any> {
     return this.firestore.collection('lista').add(immatricolazione);
+  }
+
+  getLista(): Observable<any> {
+    return this.firestore.collection('lista').snapshotChanges();
   }
 }
